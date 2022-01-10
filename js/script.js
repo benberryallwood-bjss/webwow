3dc02c6fa1f2a529afa125700e3d34840f327424
Ben Berry-Allwood <ben.berry-allwood@bjss.com>
2022-01-10 17:37:39 +0000

    Add icons (subject to change) and implement delete

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const form = document.getElementById("new-album-form");
+const table = document.getElementById("album-table");
 
 form.onsubmit = (e) => {
   e.preventDefault();
@@ -12,25 +13,72 @@ form.onsubmit = (e) => {
   form.reset();
 };
 
-function showAlbumForm() {
+const showAlbumForm = () => {
   form.style.display = "inline";
-}
+};
 
 const addAlbumToTable = (albumName, artist, year) => {
   let newRow = document.createElement("tr");
   let albumCell = document.createElement("td");
   let artistCell = document.createElement("td");
   let yearCell = document.createElement("td");
+  let iconCell = document.createElement("td");
+  let editIcon = document.createElement("i");
+  let deleteIcon = document.createElement("i");
+
+  editIcon.classList.add("icon", "fa", "fa-edit");
+  deleteIcon.classList.add("icon", "fa", "fa-remove");
 
   albumCell.innerHTML = albumName;
   artistCell.innerHTML = artist;
   yearCell.innerHTML = year;
 
+  iconCell.appendChild(editIcon);
+  iconCell.appendChild(deleteIcon);
+
+  newRow.classList.add("main-table-content");
+  iconCell.classList.add("icon-column");
+
   newRow.appendChild(albumCell);
   newRow.appendChild(artistCell);
   newRow.appendChild(yearCell);
+  newRow.appendChild(iconCell);
 
-  const table = document.getElementById("album-table");
+  addListenersToIcons(newRow);
   table.appendChild(newRow);
 };
+
+const addListenersToIcons = (row) => {
+  let editIcon = row.children[3].firstChild;
+  let deleteIcon = row.children[3].lastChild;
+
+  row.onmouseenter = () => {
+    editIcon.style.opacity = "1";
+    editIcon.style.color = "#EBCB8B";
+    deleteIcon.style.opacity = "1";
+    deleteIcon.style.color = "#BF616A";
+  };
+
+  row.onmouseleave = () => {
+    editIcon.style.opacity = "0.2";
+    editIcon.style.color = "#2E3440";
+    deleteIcon.style.opacity = "0.2";
+    deleteIcon.style.color = "#2E3440";
+  };
+
+  editIcon.onclick = (e) => {
+    console.log(e.target.parentNode.parentNode);
+  };
+
+  deleteIcon.onclick = (e) => {
+    e.target.parentNode.parentNode.remove(e.target.parentNode);
+  };
+};
+
+let tableRows = [...table.children[0].children];
+tableRows.shift();
+
+tableRows.forEach((row) => {
+  addListenersToIcons(row);
+});
 
