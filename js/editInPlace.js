06266419ecc7b7715229094930e8442e6f6c671c
Ben Berry-Allwood <ben.berry-allwood@bjss.com>
2022-01-11 14:23:27 +0000

    Add submit to in place form thing

--- js/editInPlace.js
+++ js/editInPlace.js
@@ -3,6 +3,12 @@ const getAlbumForm = (currentAlbumName = "") => {
   const input = document.createElement("input");
   input.type = "text";
   input.name = "albumName";
+
+  input.value = currentAlbumName;
+  if (input.value == "") {
+    input.placeholder = "Album name";
+  }
+
   if (!(input.value = currentAlbumName)) {
     input.placeholder = "Album name";
   }
@@ -16,6 +22,17 @@ const editRow = (row) => {
   const currentAlbumName = row.children[0].innerText;
   const cell = row.children[0];
 
-  cell.removeChild(cell.childNodes[0]);
-  cell.appendChild(getAlbumForm(currentAlbumName));
+  if (cell.firstChild) cell.removeChild(cell.firstChild);
+
+  newForm = getAlbumForm(currentAlbumName);
+  newForm.onsubmit = (e) => {
+    e.preventDefault();
+    cell.innerText = newForm["albumName"].value;
+  };
+
+  cell.appendChild(newForm);
 };
+
+// table.children[1].children[0].onclick = () => {
+//   editRow(table.children[1].children[0]);
+// };
