af0999e79ffd862e5c4ade73dc4ca75d3596af72
Ben Berry-Allwood <ben.berry-allwood@bjss.com>
2022-02-10 10:47:41 +0000

    Bugfix: Return 0 for favourite year from stub-api if no albums
    Also add more albums to stub-api

--- js/stub-api.js
+++ js/stub-api.js
@@ -31,26 +31,152 @@ const stubApi = {
 let stubbedData = [
   {
     id: '1',
-    name: 'Illmatic',
-    artist: 'Nas',
-    year: '1994',
+    name: 'Telefone',
+    artist: 'Noname',
+    year: '2016',
   },
   {
     id: '2',
-    name: 'Ready to Die',
-    artist: 'The Notorious B.I.G',
-    year: '1994',
+    name: 'Room 25',
+    artist: 'Noname',
+    year: '2018',
   },
   {
     id: '3',
-    name: 'Soundpieces: Da Antidote',
-    artist: 'Lootpack',
-    year: '1998',
+    name: 'Djesse Vol. 3',
+    artist: 'Jacob Collier',
+    year: '2020',
   },
+  {
+    id: '4',
+    name: 'BELIEVE IN ME, WHO BELIEVES IN YOU',
+    artist: 'Aries',
+    year: '2021',
+  },
+  {
+    id: '5',
+    name: 'An Evening With Silk Sonic',
+    artist: 'Bruno Mars, Anderson .Paak, Silk Sonic',
+    year: '2021',
+  },
+  {
+    id: '6',
+    name: 'Heavier Things',
+    artist: 'John Mayer',
+    year: '2003',
+  },
+  {
+    id: '7',
+    name: 'Sob Rock',
+    artist: 'John Mayer',
+    year: '2021',
+  },
+  {
+    id: '8',
+    name: 'Illuminate',
+    artist: 'Shawn Mendes',
+    year: '2017',
+  },
+  {
+    id: '9',
+    name: 'Best of Me',
+    artist: 'Cory Henry',
+    year: '2021',
+  },
+  {
+    id: '10',
+    name: 'Baby Darling Doll Face Honey',
+    artist: 'Band Of Skulls',
+    year: '2009',
+  },
+  {
+    id: '11',
+    name: 'The Making Of:',
+    artist: 'The Middle Coast',
+    year: '2017'
+  },
+  {
+    id: '12',
+    name: 'Remarkably Human',
+    artist: 'Nick Johnston',
+    year: '2016'
+  },
+  {
+    id: '13',
+    name: 'Wide Eyes in the Dark',
+    artist: 'Nick Johnston',
+    year: '2019'
+  },
+  {
+    id: '14',
+    name: 'All That You Dream',
+    artist: 'Joey Landreth',
+    year: '2021'
+  },
+  {
+    id: '15',
+    name: 'From the Reach',
+    artist: 'Sonny Landreth',
+    year: '2008'
+  },
+  {
+    id: '16',
+    name: 'How Long',
+    artist: 'Ariel Posen',
+    year: '2019'
+  },
+  {
+    id: '17',
+    name: 'Whiskey',
+    artist: 'Joey Landreth',
+    year: '2019'
+  },
+  {
+    id: '18',
+    name: 'Mile End',
+    artist: 'Ariel Posen',
+    year: '2021'
+  },
+  {
+    id: '19',
+    name: 'Royal Blood',
+    artist: 'Royal Blood',
+    year: '2014'
+  },
+  {
+    id: '20',
+    name: 'Between the Devil & the Deep Blue Sea',
+    artist: 'Black Stone Cherry',
+    year: '2011'
+  },
+  {
+    id: '21',
+    name: 'White Bear',
+    artist: 'The Temperance Movement',
+    year: '2016'
+  },
+  {
+    id: '22',
+    name: 'The Temperance Movement',
+    artist: 'The Temperance Movement',
+    year: '2013'
+  },
+  {
+    id: '23',
+    name: 'World on Fire',
+    artist: 'Slash',
+    year: '2014'
+  },
+  {
+    id: '24',
+    name: 'Apocalyptic Love',
+    artist: 'Slash',
+    year: '2012'
+  }
 ];
 
 const getMaxId = () => {
-  return Math.max(...stubbedData.map((album) => +album.id));
+  return stubbedData.length > 0 ? Math.max(...stubbedData.map((album) => +album.id)) : 0;
 };
 
 export { stubApi };
