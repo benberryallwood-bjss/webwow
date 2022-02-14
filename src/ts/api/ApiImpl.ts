09377c4b3b29d1882adf61747de165965bf9b354
Ben Berry-Allwood <ben.berry-allwood@bjss.com>
2022-02-14 15:18:30 +0000

    Change ApiImpl to typescript

--- src/ts/api/ApiImpl.ts
+++ src/ts/api/ApiImpl.ts
@@ -1,7 +1,8 @@
+import { Api } from './Api.js';
 import { stubApi } from './stub-api.js';
 import { serverApi } from './server-api.js';
 import { USE_STUBS } from './config.js';
 
-let api = USE_STUBS ? stubApi : serverApi;
+let api: Api = USE_STUBS ? stubApi : serverApi;
 
 export { api };
